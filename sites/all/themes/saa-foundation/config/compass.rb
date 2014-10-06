ed961570d33d441a5339777db0e48e5faaee12ab
SAA Phoenix <saaphx@forseti.dreamhost.com>
2014-10-05 21:47:30 -0700

    change confiog file

--- sites/all/themes/saa-foundation/config/compass.rb
+++ sites/all/themes/saa-foundation/config/compass.rb
@@ -11,13 +11,13 @@ javascripts_dir = "js"
 fonts_dir= "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = true
+ line_comments = false
 # sass_options = {:sourcemap => true }
 # enable_sourcemaps = true
 
